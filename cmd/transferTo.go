0e1e8b5610b8a6c60d29f2c1cd534f4099dc714c
Bill Glover <bill@billglover.co.uk>
2018-05-11 08:31:02 +0100

    Implement transfer to savings goal

--- cmd/transferTo.go
+++ cmd/transferTo.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"strconv"
+
+	"github.com/billglover/starling"
 
 	"github.com/google/uuid"
 
@@ -29,12 +32,25 @@ func transferTo(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 
-	amount, err := strconv.ParseFloat(args[1], 64)
+	val, err := strconv.ParseFloat(args[1], 64)
 	if err != nil {
 		fmt.Println("You must specify the amount you wish to transfer.")
 		os.Exit(1)
 	}
 
-	fmt.Println("goal:", goalID)
-	fmt.Println("amount:", amount)
+	amt := starling.Amount{
+		MinorUnits: int64(100 * val),
+		Currency:   "GBP",
+	}
+
+	ctx := context.Background()
+	sb := newClient(ctx)
+
+	result, _, err := sb.AddMoney(ctx, goalID.String(), amt)
+	if err != nil {
+		fmt.Println("Unable to transfer funds to the savings goal:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Transfer complete:", result)
 }
