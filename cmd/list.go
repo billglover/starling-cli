a7e5eab0d4bf47211d9abc574d798fc32ea1dfd2
Bill Glover <bill@billglover.co.uk>
2018-05-13 13:30:39 +0100

    Add uuid flag to display uuid for transactions

--- cmd/list.go
+++ cmd/list.go
@@ -65,9 +65,18 @@ func listTransactions() {
 		os.Exit(1)
 	}
 
-	color.Green("%3s %30s %10s %30s\n", "#", "Created", "Amount", "Narrative")
-	for i, txn := range *txns {
-		fmt.Printf("%s %30s %10.2f %30s\n", color.BlueString("%03d", i), txn.Created, txn.Amount, txn.Narrative)
+	uuid := viper.GetBool("uuid")
+
+	if uuid == true {
+		color.Green("%3s %30s %10s %30s %40s\n", "#", "Created", "Amount", "Narrative", "UUID")
+		for i, txn := range *txns {
+			fmt.Printf("%s %30s %10.2f %30s %40s\n", color.BlueString("%03d", i), txn.Created, txn.Amount, txn.Narrative, txn.UID)
+		}
+	} else {
+		color.Green("%3s %30s %10s %30s\n", "#", "Created", "Amount", "Narrative")
+		for i, txn := range *txns {
+			fmt.Printf("%s %30s %10.2f %30s\n", color.BlueString("%03d", i), txn.Created, txn.Amount, txn.Narrative)
+		}
 	}
 }
 
