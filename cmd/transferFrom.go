32f24b48d1cc9a165c359e3c604c969c3f27a59e
Bill Glover <bill@billglover.co.uk>
2018-05-16 23:43:52 +0530

    Add functionality to withdraw money from a savings goal

--- cmd/transferFrom.go
+++ cmd/transferFrom.go
@@ -1,19 +1,53 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"strconv"
 
+	"github.com/billglover/starling"
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
 
 var transferFromCmd = &cobra.Command{
 	Use:   "from",
 	Short: "A brief description of your command",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("transferFrom called")
-	},
+	Run:   transferFrom,
 }
 
 func init() {
 	transferCmd.AddCommand(transferFromCmd)
+}
+
+func transferFrom(cmd *cobra.Command, args []string) {
+
+	goalID, err := uuid.Parse(args[0])
+	if err != nil {
+		fmt.Println("You must provide the UID for the savings goal.")
+		os.Exit(1)
+	}
+
+	val, err := strconv.ParseFloat(args[1], 64)
+	if err != nil {
+		fmt.Println("You must specify the amount you wish to transfer.")
+		os.Exit(1)
+	}
+
+	amt := starling.Amount{
+		MinorUnits: int64(100 * val),
+		Currency:   "GBP",
+	}
+
+	ctx := context.Background()
+	sb := newClient(ctx)
+
+	result, _, err := sb.Withdraw(ctx, goalID.String(), amt)
+	if err != nil {
+		fmt.Println("Unable to transfer funds from the savings goal:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Transfer complete:", result)
 }
