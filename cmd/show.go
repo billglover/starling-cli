545d15f9b9769b4daefcdf7023f51e69c2197fef
Bill Glover <bill@billglover.co.uk>
2018-06-19 08:25:05 +0100

    show: display usage information on invalid command

--- cmd/show.go
+++ cmd/show.go
@@ -1,11 +1,9 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"os"
 
-	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
@@ -25,21 +23,7 @@ func init() {
 }
 
 func show(cmd *cobra.Command, args []string) {
-	ctx := context.Background()
-	sb := newClient(ctx)
-	c, _, err := sb.Card(ctx)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	key := color.New(color.FgBlue).SprintFunc()
-	fmt.Printf("%-20s %40s\n", key("Type:"), c.Type)
-	fmt.Printf("%-20s %40s\n", key("Name:"), c.NameOnCard)
-	fmt.Printf("%-20s %40s\n", key("Card Num:"), "**** **** **** "+c.LastFourDigits)
-	fmt.Printf("%-20s %40v\n", key("Enabled:"), c.Enabled)
-	fmt.Printf("%-20s %40v\n", key("Activated:"), c.Activated)
-	fmt.Printf("%-20s %40v\n", key("Cancelled:"), c.Cancelled)
-	fmt.Printf("%-20s %40s\n", key("UID:"), c.UID)
+	fmt.Printf("Error: invalid command \"%s\" provided\n", args[0])
+	cmd.Usage()
+	os.Exit(1)
 }
