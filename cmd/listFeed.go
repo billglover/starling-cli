d4a72a22f49cdbf1a89ec4bbd8d618cb85afd568
Bill Glover <bill@billglover.co.uk>
2018-07-02 21:37:16 +0100

    Add list feed command Initial work on #35 by adding a basic list command.

---
+++ cmd/listFeed.go
@@ -0,0 +1,68 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var listFeedCmd = &cobra.Command{
+	Use:   "feed",
+	Short: "List recent feed items",
+	Run:   listFeed,
+}
+
+func init() {
+	listCmd.AddCommand(listFeedCmd)
+}
+
+func listFeed(cmd *cobra.Command, args []string) {
+	if len(args) > 0 {
+		fmt.Printf("Error: invalid command \"%s\" provided\n", args[0])
+		cmd.Usage()
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	sb := newClient(ctx)
+
+	act := viper.GetString("account")
+	cat := viper.GetString("category")
+
+	items, _, err := sb.Feed(ctx, act, cat, nil)
+	check(err, "unable to list feed")
+
+	if len(items) == 0 {
+		return
+	}
+
+	limit := viper.GetInt("limit")
+	if limit > len(items) {
+		limit = len(items)
+	}
+
+	uuid := viper.GetBool("uuid")
+
+	if uuid == true {
+		color.Green("%-3s %-24s %-10s %-30s %-40s\n", "#", "Time", "Amount", "Reference", "UUID")
+		for i := 0; i < limit; i++ {
+			item := items[i]
+			fmt.Printf("%s %-24s %10.2f %-30s %-40s\n", color.BlueString("%03d", i), item.TransactionTime, float64(item.Amount.MinorUnits)/100, item.Reference, item.FeedItemUID)
+		}
+	} else {
+		color.Green("%-3s %-24s %-10s %-30s\n", "#", "Created", "Amount", "Reference")
+		for i := 0; i < limit; i++ {
+			item := items[i]
+			fmt.Printf("%s %-24s %10.2f %-30s\n", color.BlueString("%03d", i), item.TransactionTime, float64(item.Amount.MinorUnits)/100, item.Reference)
+		}
+	}
+
+	if limit < len(items) {
+		color.Set(color.FgHiMagenta)
+		fmt.Printf("%d of %d transactions\n", limit, len(items))
+	}
+}
